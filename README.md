cab42c497beb8ac7f55afc6ff3f88f3edd1a89b4
Ochieng Olanga <ochiengolanga@gmail.com>
2018-03-25 19:14:55 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # workshop-spring-boot-reactive-standalone
+
+**Hello Word controller**
+
+`http localhost:8080/greetings`
--- src/main/java/com/ochiengolanga/tuts/reactivebootstandalone/ReactivebootstandaloneApplication.java
+++ src/main/java/com/ochiengolanga/tuts/reactivebootstandalone/ReactivebootstandaloneApplication.java
@@ -2,7 +2,9 @@ package com.ochiengolanga.tuts.reactivebootstandalone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableWebFlux
 @SpringBootApplication
 public class ReactivebootstandaloneApplication {
 
