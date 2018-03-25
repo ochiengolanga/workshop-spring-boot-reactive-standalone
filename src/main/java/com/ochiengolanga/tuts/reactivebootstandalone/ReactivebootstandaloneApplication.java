90f966e98ef65ae38271fc4231e6604efab50c52
Ochieng Olanga <ochiengolanga@gmail.com>
2018-03-25 12:59:45 +0300

    Initial commit

---
+++ src/main/java/com/ochiengolanga/tuts/reactivebootstandalone/ReactivebootstandaloneApplication.java
@@ -0,0 +1,12 @@
+package com.ochiengolanga.tuts.reactivebootstandalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivebootstandaloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivebootstandaloneApplication.class, args);
+	}
+}
