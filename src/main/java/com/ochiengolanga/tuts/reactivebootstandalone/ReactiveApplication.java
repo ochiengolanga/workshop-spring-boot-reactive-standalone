84e805567939ff598bff33183327d93374df82d0
Ochieng Olanga <ochiengolanga@gmail.com>
2019-01-07 21:00:20 +0300

    Updated boot

--- src/main/java/com/ochiengolanga/tuts/reactivebootstandalone/ReactiveApplication.java
+++ src/main/java/com/ochiengolanga/tuts/reactivebootstandalone/ReactiveApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactivebootstandaloneApplication {
+public class ReactiveApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReactivebootstandaloneApplication.class, args);
+		SpringApplication.run(ReactiveApplication.class, args);
 	}
 }
